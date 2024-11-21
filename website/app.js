ab27a3e323c21ef940be8d07b42239a89123d084
abdo-mo <abdo.mo.py3@gmail.com>
2024-11-21 10:45:34 +0200

    updating the app.js file

--- website/app.js
+++ website/app.js
@@ -2,61 +2,60 @@ const apiKey = '537cfefd7c27740538249cfb4f9b810d';
 const baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?id='
 let date = new Date();
 
-document.getElementById('generate').addEventListener('click', performAction);
-
-function performAction(e) {
-    id = document.getElementById('zip').value;
-    user_res = document.getElementById('feelings').value;
-    getTemp(baseUrl,id, apiKey)
-    .then(function(allData){
-        const temp = allData.list[0].main.temp;
-        postData('http://localhost:5000/add', {temp: temp, date: date, content: user_res});
-      })
-      .then(
-        updateUI()
+function mainFunction(e) {
+  user_res = document.getElementById('feelings').value;
+  zipCode = document.getElementById('zip').value;
+    getTemp(baseUrl,zipCode, apiKey)
+    .then((returnedData) => {
+      const temp = returnedData.list[0].main.temp;
+      savingData('http://localhost:5000/add', {temp: temp, date: date, content: user_res})
+    }).then(
+        gettingUpdatedData()
       )
 }
 
-const getTemp = async (base_url, id, api_key) =>{ 
-    const url = `${base_url}${id}&appid=${api_key}`
+const getTemp = async (base_url, zipCode, api_key) =>{ 
+    const url = `${base_url}${zipCode}&appid=${api_key}`
     const request = await fetch(url);
     try {
-    const allData = await request.json()
-    return allData;
+    const returnedData = await request.json()
+    return returnedData;
     }
     catch(error) {
       console.log("error", error);
     }
   };
 
-  const postData = async ( url = '', data = {})=>{
+  const savingData = async ( url = '', obj = {})=>{
     
-    const response = await fetch(url, {
+    const rawResponse = await fetch(url, {
     method: 'POST',
-    credentials: 'same-origin', 
     headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json; charset=UTF-8',
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(obj),
   });
 
     try {
-      const newData = await response.json();
-        return newData
+      const content = await rawResponse.json();
+      console.log(content);
+      return content
     } catch(error) {
         console.log("error", error);
     }
 }
 
-const updateUI = async () => {
-  const request = await fetch('http://localhost:5000/all');
+const gettingUpdatedData = async () => {
   try{
-    const allData = await request.json();
-    document.getElementById('date').innerHTML = allData.data.date;
-    document.getElementById('temp').innerHTML = Math.round(allData.data.temp) + ' degrees';
-    document.getElementById('content').innerHTML = allData.data.content;
+    const request = await fetch('http://localhost:5000/all');
+    const allReturnedData = await request.json();
+    document.getElementById('date').innerHTML = allReturnedData.data.date;
+    document.getElementById('temp').innerHTML = `${allReturnedData.data.temp} degrees`;
+    document.getElementById('content').innerHTML = allReturnedData.data.content;
 
   }catch(error){
     console.log("error", error);
   }
 }
+
+document.getElementById('generate').addEventListener('click', mainFunction);
